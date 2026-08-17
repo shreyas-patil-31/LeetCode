fcaddd0bcf47c8f5e2f82d95ba334a1a331122c5
shreyas-patil-31 <shreyasrjpatil99@gmail.com>
2026-08-17 20:44:43 +0530

    Time: 0 ms (100%), Space: 8.9 MB (30.86%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -3,14 +3,12 @@ public:
     int scoreOfString(string s) {
         int i = 0;
         int j = i+1;
-        vector<int> v;
+        int sum = 0;
         while(j<s.size()){
-            int sum = abs((int)s[i] - (int)s[j]);
-            v.push_back(sum);
+            sum += abs((int)s[i] - (int)s[j]);
             i++;
             j++;
         }
-        int ans = accumulate(v.begin(),v.end(),0);
-        return ans;
+        return sum;
     }
 };
