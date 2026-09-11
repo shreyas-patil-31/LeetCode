8013c7e6944275983d7a8619182040b1177d02ed
shreyas-patil-31 <shreyasrjpatil99@gmail.com>
2026-09-11 19:19:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (48.28%) - LeetHub

--- 3894-traffic-signal-color/3894-traffic-signal-color.cpp
+++ 3894-traffic-signal-color/3894-traffic-signal-color.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string trafficSignal(int timer) {
-        if(timer==0){
-            return "Green";
+        if(timer>30 && timer<=90){
+            return "Red";
         }else if(timer==30){
             return "Orange";
-        }else if(timer>30 && timer<=90){
-            return "Red";
+        }else if(timer==0){
+            return "Green";
         }else{
             return "Invalid";
         }
